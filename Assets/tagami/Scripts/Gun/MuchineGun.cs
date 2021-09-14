beec0889bf8320be3bf60740e6b5cd2458482dc8
hopehopehope <hope03282000@icloud.com>
2021-09-14 15:30:19 +0900

    [update] 銃を撃てるように

---
+++ Assets/tagami/Scripts/Gun/MuchineGun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuchineGun : MonoBehaviour,IGun
+{
+    [SerializeField] GameObject bulletPrefab;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fire()
+    {
+        Instantiate(bulletPrefab, transform.position, Quaternion.LookRotation(transform.forward));
+    }
+}
---
+++ Assets/tagami/Scripts/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] GameObject shootingGunObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //発射
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            shootingGunObj.GetComponent<IGun>().Fire();
+        }
+    }
+}
