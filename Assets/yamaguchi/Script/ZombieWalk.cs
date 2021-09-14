46bd91ab31401751295c69e70d02ea39436ae558
tai <taikitpmiita@icloud.com>
2021-09-14 14:35:28 +0900

    [add]ゾンビ追加

---
+++ Assets/yamaguchi/Script/ZombieWalk.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieWalk : MonoBehaviour
+{
+    public Transform player;
+    UnityEngine.AI.NavMeshAgent nav;
+    Animator anim;
+
+    void Awake()
+    {
+       // player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag == "Player")
+        {
+            Debug.Log("hit");
+            anim.SetBool("isAttack", true);
+        }
+    }
+}
