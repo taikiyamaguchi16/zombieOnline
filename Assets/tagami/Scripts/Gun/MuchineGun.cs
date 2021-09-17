1ac9e188a9c6f61039c3c320aa277037210a26ad
hopehopehope <hope03282000@icloud.com>
2021-09-17 09:48:11 +0900

    [update] Playerの更新を行いました

--- Assets/tagami/Scripts/Gun/MuchineGun.cs
+++ Assets/tagami/Scripts/Gun/MuchineGun.cs
@@ -2,24 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MuchineGun : MonoBehaviour,IGun
+public class MuchineGun : MonoBehaviour, IGun
 {
+    [SerializeField] Transform muzzleTransform;
+
     [SerializeField] GameObject bulletPrefab;
- 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Fire()
     {
-        Instantiate(bulletPrefab, transform.position, Quaternion.LookRotation(transform.forward));
+        Instantiate(bulletPrefab, muzzleTransform.position, Quaternion.LookRotation(transform.forward));
     }
 }
--- Assets/tagami/Scripts/Player/PlayerController.cs
+++ Assets/tagami/Scripts/Player/PlayerController.cs
@@ -2,23 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] GameObject shootingGunObj;
 
+    Rigidbody rigidbody;
+    Vector3 velocityBuff;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //発射
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             shootingGunObj.GetComponent<IGun>().Fire();
         }
+
+        velocityBuff = Vector3.zero;
+        //移動
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocityBuff += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocityBuff -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocityBuff -= transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocityBuff += transform.right;
+        }
+        //Shift Boost
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            velocityBuff *= 2.0f;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = velocityBuff;
     }
 }
